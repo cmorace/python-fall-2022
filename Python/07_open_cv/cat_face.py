8a74f320156780e7a9eff6d56d23bd0b0194f3e6
Charles Morace <charles.c.morace@gmail.com>
2022-12-04 12:39:04 +0800

    add new link for google meet

--- Python/07_open_cv/cat_face.py
+++ Python/07_open_cv/cat_face.py
@@ -3,7 +3,7 @@ print("OpenCV version:", cv.__version__)
 cascade = cv.CascadeClassifier('Python/07_open_cv/cat_face_extended.xml')
 img = cv.imread("images/01_cat.webp")
 img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-cats = cascade.detectMultiScale(img_gray, scaleFactor=1.05, minNeighbors=6)
+cats = cascade.detectMultiScale(img_gray, scaleFactor=1.1, minNeighbors=4)
 print(f"{len(cats)} cat face{''if len(cats) == 1 else 's'} detected")
 for (x, y, w, h) in cats:
     cv.rectangle(img, pt1=(x, y), pt2=(x+w, y+h),
